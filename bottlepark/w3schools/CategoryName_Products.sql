4fa17a0c24436a7cb6d660be5c91a2ca1ad58176
yojulab <soqqhr@naver.com>
2022-10-18 14:14:42 +0900

    합계와 평균

--- bottlepark/w3schools/CategoryName_Products.sql
+++ bottlepark/w3schools/CategoryName_Products.sql
@@ -1,5 +1,5 @@
 -- CategoryName 가 Dairy Products, Seafood이고 
--- Price 10.00이상 50.00이하인 합계와 평균은 ?
+-- Price 10.00이상 50.00이하인 합계와 평균은?
 SELECT SUM(Price), AVG(Price) FROM Products
 WHERE CategoryID IN(SELECT CategoryID FROM Categories WHERE CategoryName IN('Dairy Products', 'Seafood'))
 AND Price BETWEEN 9.99 AND 50.01;
