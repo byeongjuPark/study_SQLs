621f769539f321dc94719d2e09d07713848d7ea5
yojulab <soqqhr@naver.com>
2022-10-18 14:38:13 +0900

    OrderID 내역과 총 갯수

--- hyunttora/w3schools/CategoryName_Products_Order.sql
+++ hyunttora/w3schools/CategoryName_Products_Order.sql
@@ -1,7 +1,22 @@
 -- 7 : CategoryName 가 Dairy Products, Seafood이고 
 -- Price 10.00이상 50.00이하인 주문  OrderID 내역과 총 갯수는 ?
---답 : 379
+
+
+--내역 
 SELECT DISTINCT OrderID 
+FROM OrderDetails
+WHERE ProductID 
+IN (SELECT ProductID 
+    FROM Products
+    WHERE CategoryID 
+     IN(SELECT CategoryID 
+        FROM Categories
+         WHERE CategoryName 
+            IN('Dairy Products', 'Seafood'))
+                AND Price BETWEEN 10 AND 50)
+
+--총 갯수 : 379개 
+SELECT COUNT(DISTINCT OrderID) 
 FROM OrderDetails
 WHERE ProductID 
 IN (SELECT ProductID 
