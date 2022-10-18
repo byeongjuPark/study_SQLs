d7ffc0492f7ccb66de55d51d03fa1e038e8602b0
byeongjuPark <bottlepark28@gmail.com>
2022-10-18 13:41:56 +0900

    1

--- bottlepark/w3schools/CategoryName_Products_Order.sql
+++ bottlepark/w3schools/CategoryName_Products_Order.sql
@@ -2,6 +2,7 @@
 -- Price 10.00이상 50.00이하인 주문  
 -- OrderID 내역과 총 갯수는 ?
 
+-- 내역
 SELECT OrderID FROM OrderDetails
 WHERE ProductID IN(SELECT ProductID 
                     FROM Products
@@ -14,3 +15,15 @@ WHERE ProductID IN(SELECT ProductID
                     AND Price BETWEEN 9.99 AND 50.01
                     )
 
+-- 총개수
+SELECT COUNT(*) FROM OrderDetails
+WHERE ProductID IN(SELECT ProductID 
+                    FROM Products
+                    WHERE CategoryID 
+                    IN(SELECT CategoryID 
+                        FROM Categories 
+                        WHERE CategoryName 
+                        IN('Dairy Products', 'Seafood')
+                        )
+                    AND Price BETWEEN 9.99 AND 50.01
+                    )
