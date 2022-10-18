b17ec7e88c8e43eea27ec6cf35c23e402ad880af
yojulab <soqqhr@naver.com>
2022-10-18 14:35:24 +0900

    합계와평균

--- hyunttora/w3schools/CategoryName_Products.sql
+++ hyunttora/w3schools/CategoryName_Products.sql
@@ -2,6 +2,7 @@
 -- Price 10.00이상 50.00이하인 합계와 평균은 ?
 -- 답 :  SUM(Price) = 390.34 / 	AVG(Price) = 24.396250
 
+SELECT SUM(Price), AVG(Price)
 FROM Products
 WHERE CategoryID IN (SELECT CategoryID 
 				FROM Categories 
