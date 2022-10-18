282b0f473f45895de0cbf1d6bcc36982f2e2f010
yojulab <soqqhr@naver.com>
2022-10-18 12:57:56 +0900

    합계와평균

---
+++ hyunttora/w3schools/CategoryName_Products.sql
@@ -0,0 +1,9 @@
+--  6 : CategoryName 가 Dairy Products, Seafood이고 
+-- Price 10.00이상 50.00이하인 합계와 평균은 ?
+-- 답 :  SUM(Price) = 390.34 / 	AVG(Price) = 24.396250
+
+FROM Products
+WHERE CategoryID IN (SELECT CategoryID 
+				FROM Categories 
+                WHERE CategoryName IN ('Dairy Products','Seafood'))
+                AND (Price BETWEEN 10 AND 50);
