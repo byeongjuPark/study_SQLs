a815a47d4483f914ecaf319425a92b0e84ad09c8
byeongjuPark <bottlepark28@gmail.com>
2022-10-20 14:07:44 +0900

    1

--- bottlepark/w3schools/Products_Quantity.sql
+++ bottlepark/w3schools/Products_Quantity.sql
@@ -1,6 +1,7 @@
 -- 제품명, 가격, 주문 갯수 표시
 
-SELECT Products.ProductName, Products.Price, OrderDetails.Quantity
+SELECT Products.ProductName,Products.Price,SUM(OrderDetails.Quantity)
 FROM Products
 INNER JOIN OrderDetails
-ON Products.ProductID = OrderDetails.ProductID;
+ON Products.ProductID  = OrderDetails.ProductID
+GROUP BY Products.ProductName
