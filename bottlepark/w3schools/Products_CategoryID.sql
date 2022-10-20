ca4566a946145b74c67da5815074db43bf45617d
byeongjuPark <bottlepark28@gmail.com>
2022-10-20 14:01:18 +0900

    1

--- bottlepark/w3schools/Products_CategoryID.sql
+++ bottlepark/w3schools/Products_CategoryID.sql
@@ -1,8 +1,8 @@
--- Products에 CategoryID가 5, 6를 표시, 내용 프로덕트 이름, 공급자 이름 출력
+-- Products에 CategoryID가 5, 6를 표시, 내용 프로덕트 이름, 공급자 이름
 SELECT Products.ProductName, Suppliers.SupplierName
 FROM Products
 INNER JOIN Suppliers
 ON Products.SupplierID = Suppliers.SupplierID
-WHERE Products.CategoryID IN(5,6);
+WHERE Products.CategoryID = 5 OR Products.CategoryID = 6;
 
         
