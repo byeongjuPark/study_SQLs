293c1b9b7bed6f663dba8ad0e803e62e0491392c
byeongjuPark <bottlepark28@gmail.com>
2022-10-20 15:03:15 +0900

    ㅋㅋ

--- bottlepark/w3schools/Users_Auth_names.sql
+++ bottlepark/w3schools/Users_Auth_names.sql
@@ -1,10 +1,6 @@
 -- 이름, 이메일, 권한이름 표시, 이름이 Teddy, Allen 만 출력 ?
--- 개판남 다시
 
-SELECT DISTINCT UA.name, UA.EMAIL, auth_names.AUTH_NAME
-FROM auth_names
-INNER JOIN (SELECT *
-FROM USERS
-INNER JOIN AUTHS 
-ON users.UNIQUE_ID = auths.UNIQUE_ID_USERS 
-AND users.name IN('Teddy', 'Allen')) as UA
+SELECT users.NAME, users.EMAIL, auth_names.AUTH_NAME
+FROM (( users INNER JOIN auths ON users.UNIQUE_ID = auths.UNIQUE_ID_USERS )
+INNER JOIN auth_names ON auths.UNIQUE_ID_AUTH_NAMES = auth_names.UNIQUE_ID )
+WHERE NAME IN ( 'Teddy', 'Allen' );
